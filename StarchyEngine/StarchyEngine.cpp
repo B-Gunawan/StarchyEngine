e1c4e1a40c6f41075bcffe31ebe818405c9fb15e
Bryan <bryan.varic@gmail.com>
2020-12-04 21:25:27 +1100

    Add project files.

---
+++ StarchyEngine/StarchyEngine.cpp
@@ -0,0 +1,12 @@
+﻿// StarchyEngine.cpp : Defines the entry point for the application.
+//
+
+#include "StarchyEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
